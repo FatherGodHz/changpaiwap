51fe6f11604ea7f2466af1f75cdc145f6dbb96dc
FatherGodHz <fathergodhz@FatherGodHzdeiMac.local>
2018-06-21 17:38:06 +0800

    Test Vuex

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,28 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <button @click="changeMsg">点击改变msg</button>
+    <p> 我是App.vue中的p， gettersMsg 的值是----- {{gettersMsg}}</p>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
-  name: 'App'
+  name: 'App',
+  computed: {...mapGetters(['gettersMsg'])},
+  methods: {...mapActions(['changeMsg'])}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
